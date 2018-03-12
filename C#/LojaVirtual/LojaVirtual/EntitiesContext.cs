7ef953e6b9266ec4082b212e587ac0af480a089e
Victor <jvdelicia123@outlook.com>
2018-03-12 08:45:25 -0300

    DS 12/03

---
+++ C#/LojaVirtual/LojaVirtual/EntitiesContext.cs
@@ -0,0 +1,18 @@
+﻿using LojaVirtual.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual {
+    public class EntitiesContext : DbContext {
+        public DbSet<User> Users { get; set; }
+
+        //NuGet Console:
+        //Install-Package EntityFramework.MicrosoftSqlserver -Version 7.0.0-rc1-final -Pre
+        //Install-Package EntityFramework.Commands -Version 7.0.0-rc1-final -Pre
+        protected override void 
+    }
+}
--- C#/LojaVirtual/LojaVirtual/Program.cs
+++ C#/LojaVirtual/LojaVirtual/Program.cs
@@ -8,11 +8,21 @@ using LojaVirtual.Entities;
 namespace LojaVirtual {
     class Program {
         static void Main(string[] args) {
+            //string conn = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\dbLojaVirtual.mdf;Integrated Security=True;Connect Timeout=30"; //String de conexão
+            //IDbConnection con = new SqlConnection(conn);
+            //IDbCommand com = con.CreateCommand();
+            //com.CommandText = "select * from tbCliente";
+            //IDataReader read = com.ExecuteReader();
+            //while (read.Read()) {
+            //    int Id = Convert.ToInt32(read["Id"]);
+            //    string Name = Convert.ToString(read["Name"]);
+            //}
 
-            User Astrogildo = new User() {
-                Name = "Astrogildo",
-                Pass = "123"
-            };
+            EntitiesContext context = new EntitiesContext();
+            User Astrogildo = new User() { Name = "Astrogildo", Pass = "123" };
+            context.Users.Add(Astrogildo);
+            context.SaveChanges();
+            context.Dispose(); //Libera do cache
 
             Console.WriteLine( "Usuário salvo." );
             Console.ReadLine();
