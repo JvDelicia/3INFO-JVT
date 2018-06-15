34b625ea0ef81465f4fc0c56c1fa894b166222ce
Victor <jvdelicia123@outlook.com>
2018-06-15 12:06:21 -0300

    ASP.NET with MySQL Connection

--- ASPNET/LojaWeb/LojaWeb/DAO/EntityContext.cs
+++ ASPNET/LojaWeb/LojaWeb/DAO/EntityContext.cs
@@ -1,22 +1,21 @@
 ﻿using LojaWeb.Models;
-using Microsoft.Data.Entity;
 using MySql.Data.Entity;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace LojaWeb.DAO {
+
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+
     public class EntityContext : DbContext {
         public DbSet<Product> Products { get; set; }
         public DbSet<ProdCategory> Categories { get; set; }
         public DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            string stringConn = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
-            optionsBuilder.(stringConn);
-            base.OnConfiguring(optionsBuilder);
-        }
+        public EntityContext() : base("dblojawebConn") { }
     }
 }
