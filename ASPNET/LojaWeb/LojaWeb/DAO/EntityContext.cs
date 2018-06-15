d1790451f35284da0ce9dfaef492ba00ad4e3ba5
Victor <jvdelicia123@outlook.com>
2018-06-15 14:53:01 -0300

    ASP.NET MySql Error on DbContext

--- ASPNET/LojaWeb/LojaWeb/DAO/EntityContext.cs
+++ ASPNET/LojaWeb/LojaWeb/DAO/EntityContext.cs
@@ -1,21 +1,23 @@
 ﻿using LojaWeb.Models;
+using Microsoft.Data.Entity.Infrastructure;
 using MySql.Data.Entity;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
 namespace LojaWeb.DAO {
 
-    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class EntityContext: DbContext {
 
-    public class EntityContext : DbContext {
         public DbSet<Product> Products { get; set; }
         public DbSet<ProdCategory> Categories { get; set; }
         public DbSet<User> Users { get; set; }
 
-        public EntityContext() : base("dblojawebConn") { }
+        //https://dev.mysql.com/doc/connector-net/en/connector-net-entityframework60.html
+        //https://dev.mysql.com/doc/connector-net/en/connector-net-entity-framework.html
     }
 }
