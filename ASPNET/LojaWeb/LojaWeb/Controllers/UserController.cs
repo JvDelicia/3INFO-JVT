3eacb48e70fe648f500ac9ea6ac04009e526e13f
Victor <jvdelicia123@outlook.com>
2018-06-20 16:28:00 -0300

    MySQL + ASP.NET MVC5 EF6 100% Funcional

---
+++ ASPNET/LojaWeb/LojaWeb/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using LojaWeb.DAO;
+using LojaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaWeb.Controllers
+{
+    public class UserController : Controller
+    {
+		public object UserDao { get; private set; }
+
+		// GET: User
+		public ActionResult Index() {
+
+			return View();
+		}
+
+		public ActionResult Form() {
+
+			return View();
+		}
+
+		public ActionResult Add(User u) {
+			UserDAO udao = new UserDAO();
+			udao.Insert(u);
+			return View();
+		}
+	}
+}
