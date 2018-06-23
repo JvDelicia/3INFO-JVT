76707cf5cce4afbd4069524eccf1f629b2330c06
Victor <jvdelicia123@outlook.com>
2018-06-22 21:56:43 -0300

    Installed Bootstrap and FontAwesome

--- ASPNET/LojaWeb/LojaWeb/App_Start/RouteConfig.cs
+++ ASPNET/LojaWeb/LojaWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LojaWeb
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "User", action = "Form", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
 		}
     }
