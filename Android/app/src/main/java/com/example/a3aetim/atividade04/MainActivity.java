ad0d7fdc2e5da5e403744c06cb3681f05d79e40d
3A Etim Info <3aetim@basilides.local>
2018-08-21 11:14:33 -0300

    Alguns do ASPNET e Java/Andoid pt.1

--- Android/app/src/main/java/com/example/a3aetim/atividade04/MainActivity.java
+++ Android/app/src/main/java/com/example/a3aetim/atividade04/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a3aetim.atividade04;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //as coisas da activity
+
+        helper = new DatabaseHelper(this);
+    }
+
+    public void Salvar(View view) {
+        SQLiteDatabase db = helper.getWriteableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("atributo", valor);
+        long result = db.insert("banco", null, values);
+        if (result != -1) {
+            Toast.makeText(this, "Registro salvo", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Erro ao salvar", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        helper.close();
+        super.onDestroy();
     }
 }
