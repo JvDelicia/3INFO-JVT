87048378560721fbe95976580f6739fa8a627bd2
Victor <jvdelicia123@outlook.com>
2018-08-31 22:28:07 -0300

    LojaWeb update

---
+++ ASPNET/LojaWeb/LojaWeb/Models/Sell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LojaWeb.Models {
+	public class Sell { // Falta 2 atributos
+		public int Id { get; set; }
+		public decimal TotalPrice { get; set; }
+		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+		public DateTime Date { get; set; }
+		public User User { get; set; }
+		public int UserId { get; set; }
+	}
+}
--- ASPNET/LojaWeb/LojaWeb/Models/User.cs
+++ ASPNET/LojaWeb/LojaWeb/Models/User.cs
@@ -8,5 +8,8 @@ namespace LojaWeb.Models {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Pass { get; set; }
+        public string Phone { get; set; }
+        public DateTime Born { get; set; }
+        public string Email { get; set; }
     }
 }
