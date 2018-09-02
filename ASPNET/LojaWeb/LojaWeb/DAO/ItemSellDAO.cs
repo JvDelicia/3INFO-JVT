145dc53b3b75bcc46470e9cd9690bd046ad9313c
Victor <jvdelicia123@outlook.com>
2018-09-02 14:09:36 -0300

    ECommerce is a trash

---
+++ ASPNET/LojaWeb/LojaWeb/DAO/ItemSellDAO.cs
@@ -0,0 +1,37 @@
+﻿using LojaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaWeb.DAO {
+	public class ItemSellDAO {
+		private EntityContext context;
+
+		public ItemSellDAO() {
+			context = new EntityContext();
+		}
+
+		public IList<ItemSell> ItemSellList() {
+			return context.ItemSells.ToList();
+		}
+
+		public ItemSell FindById(int id) {
+			return context.ItemSells.FirstOrDefault(u => u.Id == id);
+		}
+
+		public void Insert(ItemSell isell) {
+			context.ItemSells.Add(isell);
+			context.SaveChanges();
+		}
+
+		public void Remove(ItemSell isell) {
+			context.ItemSells.Remove(isell);
+			context.SaveChanges();
+		}
+
+		public void Update() {
+			context.SaveChanges();
+		}
+	}
+}
--- ASPNET/LojaWeb/LojaWeb/Models/Sell.cs
+++ ASPNET/LojaWeb/LojaWeb/Models/Sell.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Web;
 
 namespace LojaWeb.Models {
-	public class Sell { // Falta 2 atributos
+	public class Sell {
 		public int Id { get; set; }
 		public decimal TotalPrice { get; set; }
 		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
 		public DateTime Date { get; set; }
+		public virtual ICollection<ItemSell> ItemProd { get; set; }
 		public User User { get; set; }
 		public int UserId { get; set; }
+		public string Status { get; set; }
 	}
 }
