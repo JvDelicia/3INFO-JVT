cff6080135a4005504b44ebaf8b530cd9a778839
Victor <jvdelicia123@outlook.com>
2018-09-03 02:28:57 -0300

    1 ecommerce

---
+++ ASPNET/Ecommerce/Ecommerce/Models/EntityContext.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Ecommerce.Models {
+	[DbConfigurationType(typeof(MySqlEFConfiguration))]
+	public class EntityContext : DbContext {
+		public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<ProdutoVenda> ProdutoVenda { get; set; }
+
+		public EntityContext() : base("DBCommerce") { }
+		public EntityContext(DbConnection existingConnection, bool contextOwnsConnection) : base(existingConnection, contextOwnsConnection) { }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
